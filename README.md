474ba4f8fd7c79fd6175a2a2ae4318f2f462f101
eMasumBillah <emasumbillah@gmail.com>
2023-08-05 09:27:58 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# e-com is a online shop
+
+# Express server
+
+# HTTP request & response
+
+# nodemon & morgan (v5)
+
+# API test with postman (v6)
+
+# Middleware (v7)
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+const isLoggedIn = (res, req, next) => {
+  console.log('Loged In');
+  next();
+};
+
+app.get('/product', (req, res) => {
+  res.status(200).send({ message: 'GET product information' });
+});
+
+app.get('/user', isLoggedIn, (req, res) => {
+  res.status(200).send({ message: 'User profile is returned' });
+});
+
+app.listen(5000, () => {
+  console.log(`server is running at http://localhost:5000`);
+});
